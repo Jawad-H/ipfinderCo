a57b4ce1935956bde9ae25190506c301331ac124
DESKTOP-A4DPR99\Jawad Hussain <jawwadhussain0@gmail.com>
2022-07-04 01:24:57 -0700

    lib removed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from 'react'
+import { React, useState } from 'react'
 import styled from "styled-components";
 import { BiWorld } from "react-icons/bi";
 import { FaCity, FaSearchLocation } from "react-icons/fa";
